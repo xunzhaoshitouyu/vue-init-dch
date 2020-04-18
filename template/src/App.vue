7514f1172b8d346c3a82f1fe6c63a19ac2c56b39
chenchen <chenchent@dchealth.com>
2020-04-18 22:33:57 +0800

    App.vue修改

--- template/src/App.vue
+++ template/src/App.vue
@@ -2,24 +2,24 @@
   <div id="app">
     <img src="./assets/logo.png">
     {{#router}}
-    <router-view/>
+    <router-view />
     {{else}}
-    <HelloWorld/>
+    <HelloWorld />
     {{/router}}
   </div>
 </template>
 
 <script>
 {{#unless router}}
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld"
 
 {{/unless}}
 export default {
-  name: 'App'{{#router}}{{else}},
+  name: "App"{{#router}}{{else}},
   components: {
     HelloWorld
   }{{/router}}
-}
+};
 </script>
 
 <style>
